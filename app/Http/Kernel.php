6bdce71753900b19d727e49ca3fc68fa36722c7d
Kumar Gaurav <54886848+skygaurav@users.noreply.github.com>
2025-12-27 18:27:52 +0000

    Not found issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,12 +12,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
@@ -28,11 +28,11 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\AddQueuedCookiesToResponse::class,
+            \App\Http\Middleware\StartSession::class,
+            \App\Http\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
---
+++ app/Http/Middleware/ValidatePostSize.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\ValidatePostSize as Middleware;
+
+class ValidatePostSize extends Middleware
+{
+}
