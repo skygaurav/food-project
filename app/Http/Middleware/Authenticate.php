26556171b98f6cf7202f6d8468587ec4b0aa0cad
Kumar Gaurav <54886848+skygaurav@users.noreply.github.com>
2025-12-28 11:46:31 +0000

    upload page issue

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,16 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
-        if ($request->expectsJson()) {
-            return null;
+        if (!$request->expectsJson()) {
+            return '/login';
         }
-
-        return '/login';
     }
 }
